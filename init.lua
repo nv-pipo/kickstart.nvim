de77d55e222f7b96613369636f8e57ec480342b9
Nicolas Velasquez <gnicolax@gmail.com>
2024-08-21 12:35:59 +0200

    feat: Add keybindings

--- init.lua
+++ init.lua
@@ -161,6 +161,39 @@ vim.opt.scrolloff = 10
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
+-- Set backspace to be more intuitive
+vim.opt.backspace = { 'indent', 'eol', 'start' }
+
+-- Use Meta + hl to switch between tabs, and new tab with Meta+Cmd + t, close with Meta+w
+vim.keymap.set('n', '<M-t>', '<cmd>tabnew<CR>', { desc = 'New tab' })
+vim.keymap.set('n', '<M-h>', '<cmd>tabprevious<CR>', { desc = 'Switch to previous tab' })
+vim.keymap.set('n', '<M-l>', '<cmd>tabnext<CR>', { desc = 'Switch to next tab' })
+vim.keymap.set('n', '<M-w>', '<cmd>tabclose<CR>', { desc = 'Close tab' })
+
+-- Copilot accept next word
+vim.keymap.set('i', '<M-f>', '<Plug>(copilot-accept-word)')
+vim.keymap.set('i', '<M-DOWN>', '<Plug>(copilot-accept-line)')
+
+-- Text to speach visually selected text
+local start_tts = function()
+  -- Save the visually selected text to register 's'
+  vim.cmd.norm '"sy'
+  -- Get the text from register 's'
+  local text = vim.fn.getreg 's'
+  -- Speak the text with a rate of 200 words per minutes
+  vim.system { 'say', text, '-r 200' }
+end
+vim.keymap.set('v', '<leader>s', start_tts, { desc = '[S]peak visually selected text' })
+
+-- Stop speaking
+local stop_tts = function()
+  vim.system { 'pkill', 'say' }
+end
+vim.keymap.set('n', '<leader>ks', stop_tts, { desc = '[K]ill [S]peak visually selected text' })
+
+-- Set up keybinding for select all
+vim.keymap.set('n', '<D-a>', 'G$vgg0', { desc = 'Select all' })
+
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
@@ -282,6 +315,7 @@ require('lazy').setup({
       require('which-key').add {
         { '<leader>c', group = '[C]ode' },
         { '<leader>d', group = '[D]ocument' },
+        { '<leader>k', group = '[K]ill' },
         { '<leader>r', group = '[R]ename' },
         { '<leader>s', group = '[S]earch' },
         { '<leader>w', group = '[W]orkspace' },
@@ -370,6 +404,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>sa', ':lua require"telescope.builtin".find_files({ hidden = true })<CR>', { desc = '[S]earch [A]ll Files' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
