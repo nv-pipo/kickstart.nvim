1d3009004ef80a7aff3c1aaae9ccc503b366ac3e
Nicolas Velasquez <gnicolax@gmail.com>
2024-08-21 15:47:12 +0200

    chore: Enable live preview in options.lua

--- lua/custom/core/options.lua
+++ lua/custom/core/options.lua
@@ -4,5 +4,8 @@ vim.opt.relativenumber = true
 -- Set backspace to be more intuitive
 vim.opt.backspace = { 'indent', 'eol', 'start' }
 
+-- Live preview
+vim.opt.inccommand = 'split'
+
 -- Don't have 'o' add a comment
 vim.opt.formatoptions:remove 'o'
