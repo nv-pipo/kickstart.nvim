0f36780f619e3c0dff5fa679cb01e810d3f7ac52
Nicolas Velasquez <gnicolax@gmail.com>
2024-08-21 15:47:03 +0200

    chore: Remove 'o' from formatoptions in options.lua

--- lua/custom/core/options.lua
+++ lua/custom/core/options.lua
@@ -3,3 +3,6 @@ vim.opt.relativenumber = true
 
 -- Set backspace to be more intuitive
 vim.opt.backspace = { 'indent', 'eol', 'start' }
+
+-- Don't have 'o' add a comment
+vim.opt.formatoptions:remove 'o'
