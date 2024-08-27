6f2afd126ea08de34afd6b5135e8e2a1ae69a3cf
Nicolas Velasquez <gnicolax@gmail.com>
2024-08-27 11:03:35 +0200

    feat: Use the more generic vim.keymap.set lua method

--- ftplugin/http.lua
+++ ftplugin/http.lua
@@ -1,2 +1,2 @@
 -- Add keybinding for kulala (REST client)
-vim.api.nvim_buf_set_keymap(0, 'n', '<S-CR>', ":lua require('kulala').run()<CR>", { noremap = true, silent = true, desc = 'Do HTTP request with kulala' })
+vim.keymap.set('n', '<S-CR>', ":lua require('kulala').run()<CR>", { noremap = true, silent = true, desc = 'Do HTTP request with kulala', buffer = 0 })
