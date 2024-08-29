1cac1f2eb65fe3efb577eb1a4b46df9418726f7f
Nicolas Velasquez <gnicolax@gmail.com>
2024-08-29 12:16:33 +0200

    fix: Import core modules when running nvim outside config directory

--- lua/custom/core/init.lua
+++ lua/custom/core/init.lua
@@ -1,4 +1,4 @@
-for filename in io.popen([[ls lua/custom/core/*.lua | grep -v init.lua]]):lines() do
-  local module_name = string.match(filename, '([^/]+).lua')
+for filename in io.popen('ls ' .. vim.fn.stdpath 'config' .. '/lua/custom/core/*.lua | grep -v init.lua'):lines() do
+  local module_name = string.match(filename, '([^/]+)[.]lua')
   require('custom.core.' .. module_name)
 end
