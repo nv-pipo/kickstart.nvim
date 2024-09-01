74d88133e496c7b93daeb07444f03dc26b7a58df
Nicolas Velasquez <gnicolax@gmail.com>
2024-09-01 11:36:25 +0200

    feat: Add keybindings for commenting lines and selections with CMD+/

--- lua/custom/core/keymaps.lua
+++ lua/custom/core/keymaps.lua
@@ -10,6 +10,11 @@ vim.keymap.set('i', '<M-j>', '<Plug>(copilot-accept-line)')
 -- Set up keybinding for select all
 vim.keymap.set('n', '<C-space>4', 'G$vgg0', { desc = 'Select all' })
 
+-- Comemnt line while in insert mode (by exiting insert mode)
+vim.keymap.set('i', '<C-space>5', '<Esc>gcc', { desc = 'Comment line', remap = true })
+vim.keymap.set('n', '<C-space>5', 'gcc', { desc = 'Comment line', remap = true })
+vim.keymap.set('v', '<C-space>5', 'gcgv', { desc = 'Comment selection', remap = true })
+
 -- Add keybinding to duplicate line up and down
 vim.keymap.set('n', '<M-J>', 'mz"zVyP`z', { desc = 'Duplicate current line down' })
 vim.keymap.set('n', '<M-K>', 'mz"zVyP`z:move .-2<CR>', { desc = 'Duplicate current line up' })
