e8323fdfc3fc1da111910ad54a90fa11c1fd7dcf
Nicolas Velasquez <gnicolax@gmail.com>
2024-09-04 08:47:57 +0200

    feat: Add http TS requirement

--- init.lua
+++ init.lua
@@ -908,7 +908,7 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'http' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
