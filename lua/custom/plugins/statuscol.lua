43b02a8117b092a62f599d6cc11a5921dcdc1fbe
Nicolas Velasquez <gnicolax@gmail.com>
2024-09-05 17:03:12 +0200

    feat: Add statuscol plugin for customizing statusline

---
+++ lua/custom/plugins/statuscol.lua
@@ -0,0 +1,16 @@
+return {
+  'luukvbaal/statuscol.nvim',
+  config = function()
+    local builtin = require 'statuscol.builtin'
+    require('statuscol').setup {
+      relculright = true,
+      segments = {
+        { text = { '%s' }, click = 'v:lua.ScSa' },
+        { sign = { name = { 'Dap' }, maxwidth = 1, colwidth = 2, auto = false }, click = 'v:lua.ScSa' },
+        { text = { builtin.lnumfunc, ' ' }, condition = { true, builtin.not_empty }, click = 'v:lua.ScLa' },
+        { text = { builtin.foldfunc }, click = 'v:lua.ScFa' },
+        { sign = { name = { 'lsdkfj' }, maxwidth = 1, colwidth = 1, auto = false } },
+      },
+    }
+  end,
+}
