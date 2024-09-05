d5d35acc24294378e2c35727c37a27fe9b1e73f2
Nicolas Velasquez <gnicolax@gmail.com>
2024-09-05 22:13:43 +0200

    feat: statuscol: split diagnostics from git

--- lua/custom/plugins/statuscol.lua
+++ lua/custom/plugins/statuscol.lua
@@ -5,10 +5,11 @@ return {
     require('statuscol').setup {
       relculright = true,
       segments = {
-        { text = { '%s' }, click = 'v:lua.ScSa' },
-        { sign = { name = { 'Dap' }, maxwidth = 1, colwidth = 2, auto = false }, click = 'v:lua.ScSa' },
+        { sign = { namespace = { 'diagnostic/signs' }, colwidth = 2, maxwidth = 1, auto = true }, click = 'v:lua.ScSa' },
+        { sign = { name = { 'Dap' }, maxwidth = 1, colwidth = 2, auto = true }, click = 'v:lua.ScSa' },
         { text = { builtin.lnumfunc, ' ' }, condition = { true, builtin.not_empty }, click = 'v:lua.ScLa' },
-        { text = { ' ', builtin.foldfunc, ' ' }, click = 'v:lua.ScFa' },
+        { sign = { namespace = { 'gitsigns' }, name = { '.*' }, maxwidth = 1, colwidth = 1, auto = false }, click = 'v:lua.ScSa' },
+        { text = { builtin.foldfunc, ' ' }, click = 'v:lua.ScFa' },
       },
     }
   end,
