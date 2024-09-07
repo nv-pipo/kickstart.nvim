345fb8baa6d2cbfb50333c3878c7c8c39d774001
Nicolas Velasquez <gnicolax@gmail.com>
2024-09-07 06:59:11 +0200

    feat: Fixed statuscol for diagnostics

--- lua/custom/plugins/statuscol.lua
+++ lua/custom/plugins/statuscol.lua
@@ -5,7 +5,7 @@ return {
     require('statuscol').setup {
       relculright = true,
       segments = {
-        { sign = { namespace = { 'diagnostic/signs' }, colwidth = 2, maxwidth = 1, auto = true }, click = 'v:lua.ScSa' },
+        { sign = { namespace = { 'diagnostic/signs' }, colwidth = 2, maxwidth = 1, auto = false }, click = 'v:lua.ScSa' },
         { sign = { name = { 'Dap' }, maxwidth = 1, colwidth = 2, auto = true }, click = 'v:lua.ScSa' },
         { text = { builtin.lnumfunc, ' ' }, condition = { true, builtin.not_empty }, click = 'v:lua.ScLa' },
         { sign = { namespace = { 'gitsigns' }, name = { '.*' }, maxwidth = 1, colwidth = 1, auto = false }, click = 'v:lua.ScSa' },
