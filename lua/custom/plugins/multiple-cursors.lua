4a8b98df87aba9e1317e5968be9632a4d764f8b4
Nicolas Velasquez <gnicolax@gmail.com>
2024-09-12 11:36:21 +0200

    refactor: Update keybindings for multiple-cursors plugin

--- lua/custom/plugins/multiple-cursors.lua
+++ lua/custom/plugins/multiple-cursors.lua
@@ -3,11 +3,11 @@ return {
   version = '*', -- Use the latest tagged version
   opts = {}, -- This causes the plugin setup function to be called
   keys = {
-    { '<C-Space>1', '<Cmd>MultipleCursorsAddUp<CR>', mode = { 'n', 'i', 'x' }, desc = 'Add cursor and move up' },
-    { '<C-Space>2', '<Cmd>MultipleCursorsAddDown<CR>', mode = { 'n', 'i', 'x' }, desc = 'Add cursor and move down' },
+    { '<M-ķ>', '<Cmd>MultipleCursorsAddUp<CR>', mode = { 'n', 'i', 'x' }, desc = 'Add cursor and move up' },
+    { '<M-ĵ>', '<Cmd>MultipleCursorsAddDown<CR>', mode = { 'n', 'i', 'x' }, desc = 'Add cursor and move down' },
 
     { '<M-LeftMouse>', '<Cmd>MultipleCursorsMouseAddDelete<CR>', mode = { 'n', 'i' }, desc = 'Add or remove cursor' },
 
-    { '<C-Space>3', '<Cmd>MultipleCursorsAddJumpNextMatch<CR>', mode = { 'n', 'v' }, desc = 'Add cursor and jump to next cword' },
+    { '<M-ď>', '<Cmd>MultipleCursorsAddJumpNextMatch<CR>', mode = { 'n', 'x' }, desc = 'Add cursor and jump to next cword' },
   },
 }
