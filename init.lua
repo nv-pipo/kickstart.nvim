2f857ecb1a35e604bb206e34fc9eb25305cb8f32
Nicolas Velasquez <gnicolax@gmail.com>
2024-09-13 16:42:04 +0200

    feat: Add formatter for xml

--- init.lua
+++ init.lua
@@ -713,6 +713,7 @@ require('lazy').setup({
         html = { 'prettierd' },
         json = { 'jq' },
         yaml = { 'yq' },
+        xml = { 'xmllint' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
