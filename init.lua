52172c5655f52c8b9a460bd74930df64a29626cc
Nicolas Velasquez <gnicolax@gmail.com>
2024-09-15 21:44:39 +0200

    feat: Update Telescope search files defaults

--- init.lua
+++ init.lua
@@ -370,7 +370,12 @@ require('lazy').setup({
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set(
+        'n',
+        '<leader>sf',
+        ':lua require"telescope.builtin".find_files({ find_command = { "rg", "--files", "--hidden", "-g","!.git" } })<CR>',
+        { desc = '[S]earch [F]iles' }
+      )
       vim.keymap.set(
         'n',
         '<leader>sa',
