dd214855a5b5724d14a8f2bed1edcac77a1d540b
Nicolas Velasquez <gnicolax@gmail.com>
2024-10-08 08:44:56 +0200

    feat: Switch copilot to zbirenbaum/copilot.lua

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,3 +1,35 @@
 return {
-  'github/copilot.vim',
+  'zbirenbaum/copilot.lua',
+  cmd = 'Copilot',
+  event = 'InsertEnter',
+  config = function()
+    require('copilot').setup {
+      panel = {
+        enable = false,
+      },
+      suggestion = {
+        auto_trigger = true,
+        keymap = {
+          accept = '<tab>',
+          accept_word = '<M-l>',
+          accept_line = '<M-j>',
+          next = '<M-]>',
+          prev = '<M-[>',
+          dismiss = '<C-]>',
+        },
+      },
+      filetypes = {
+        yaml = false,
+        markdown = false,
+        help = false,
+        gitcommit = false,
+        gitrebase = false,
+        hgcommit = false,
+        svn = false,
+        cvs = false,
+        python = true,
+        ['.'] = false,
+      },
+    }
+  end,
 }
