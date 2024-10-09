34a76728965f2becc89f3195be390c70381a73a1
Nicolas Velasquez <gnicolax@gmail.com>
2024-10-09 21:58:41 +0200

    refactor: Update commit prompt to show function context

--- lua/custom/plugins/copilot_chat.lua
+++ lua/custom/plugins/copilot_chat.lua
@@ -13,7 +13,7 @@ return {
         CommitStagedCustom = {
           prompt = "Write 10 commit messages for the change with commitizen convention. Make sure the title has maximum 50 characters and no message. Keep in mind you will suggest 10 commit messages, but only 1 will be used. It's better to push yourself (esp to synthesize to a higher level) and maybe wrong about some of the 10 commits because only one needs to be good. I'm looking for your best commit, not the best average commit. It's better to cover more scenarios than include a lot of overlap. Respond with one message per line",
           selection = function(_)
-            local diff_part = io.popen('git diff --no-color --no-ext-diff --staged'):read '*a'
+            local diff_part = io.popen('git diff --no-color --no-ext-diff --staged --function-context'):read '*a'
 
             if not diff_part or diff_part == '' then
               vim.notify('No staged changes to commit', vim.log.levels.INFO, { title = 'CopilotChat' })
